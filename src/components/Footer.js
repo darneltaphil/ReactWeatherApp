e54c98e624dd77c70ebe6769e8406df8f947fbb1
darneltaphil <darneltaphil@gmail.com>
2020-09-25 19:04:04 +0000

    Remove Social media footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,22 +11,13 @@ function Footer() {
 								<input type="submit" value="Subscribe"/>
 							</form>
 						</div>
-						<div className="col-md-3 col-md-offset-1">
-							<div className="social-links">
-								<a href="#"><i className="fa fa-facebook"></i></a>
-								<a href="#"><i className="fa fa-twitter"></i></a>
-								<a href="#"><i className="fa fa-google-plus"></i></a>
-								<a href="#"><i className="fa fa-pinterest"></i></a>
-							</div>
-						</div>
+						
 					</div>
 
 					<center><p className="colophon">Copyright 2020 PreMEST - Weather App Project.</p>
 					<p className="colophon">Designed by Arnold Kofi  <em>(Nitro)</em>. All rights reserved</p></center>
 				</div>
 			</footer> 
-
-
   );
 }
 
