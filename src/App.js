9fb1358126f24076b4df21601be16f997d985c35
darneltaphil <darneltaphil@gmail.com>
2020-10-15 17:16:54 +0000

    Fixing Cross-origin Request issues

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Weatherdisplay from './components/Weatherdisplay'
 import axios from 'axios'
 import './components/style.css'
 
-const baseUrl="https://weather-backend-29.herokuapp.com/"
+const baseUrl="https://weather-backend-29.herokuapp.com"
 
  class App extends React.Component {
 	 
@@ -41,7 +41,7 @@ const baseUrl="https://weather-backend-29.herokuapp.com/"
 	getWeather = ()=>{
 			const theTownLocation = (this.state.value==='')||(this.state.value===undefined)?"Accra":this.state.value
 			//axios.get(baseUrl+theTownLocation)
-			axios.get(baseUrl+"/getWeather/"+theTownLocation)  
+			axios.get("/getWeather/"+theTownLocation)  
 			.then(response => {
 				//Control Response
 				console.log(response)
